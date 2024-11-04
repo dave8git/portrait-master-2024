84e6c84e55c04e473154ac438700b7dbe6ae0a7a
dave8git <dave8git@gmail.com>
2024-11-04 20:01:24 +0100

    Add code that prohibits voting twice

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -1,9 +1,11 @@
 const Photo = require('../models/photo.model');
+const requestIp = require('request-ip');
+const Voter = require('../models/Voter.model');
 
 /****** SUBMIT PHOTO ********/
 
 const escape = html => {
-  return html 
+  return html
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
@@ -22,7 +24,7 @@ exports.add = async (req, res) => {
 
     const file = req.files.file;
 
-    if(correctTitle && correctAuthor && correctEmail && file) { // if fields are not empty...
+    if (correctTitle && correctAuthor && correctEmail && file) { // if fields are not empty...
 
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];
@@ -39,18 +41,18 @@ exports.add = async (req, res) => {
         return res.status(400).json({ message: 'Author name exceeds the 50 character limit!' });
       }
       //if(fileExt === 'jpg' || fileExt === 'png' || fileExt === 'gif') {
-        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-        await newPhoto.save(); // ...save new photo in DB
-        res.json(newPhoto);
-     //} else {
+      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+      await newPhoto.save(); // ...save new photo in DB
+      res.json(newPhoto);
+      //} else {
       //  throw new Error('Wrong file type!');
       //}
-    
+
     } else {
       throw new Error('Wrong input!');
     }
 
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 
@@ -62,7 +64,7 @@ exports.loadAll = async (req, res) => {
 
   try {
     res.json(await Photo.find());
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 
@@ -71,16 +73,25 @@ exports.loadAll = async (req, res) => {
 /****** VOTE FOR PHOTO ********/
 
 exports.vote = async (req, res) => {
-
+  const voterIp = requestIp.getClientIp(req);
   try {
+    let voter = await Voter.findOne({ user: voterIp });
+    if (!voter) {
+      voter = new Voter({ user: voterIp });
+    }
     const photoToUpdate = await Photo.findOne({ _id: req.params.id });
-    if(!photoToUpdate) res.status(404).json({ message: 'Not found' });
+    if (!photoToUpdate) res.status(404).json({ message: 'Not found' });
     else {
-      photoToUpdate.votes++;
-      photoToUpdate.save();
+      if (!voter.votes.includes(photoToUpdate.id)) {
+        photoToUpdate.votes++;
+        voter.votes.push(photoToUpdate.id);
+        photoToUpdate.save();
+      } else {
+        res.json({ msg: 'What makes you think you will vote twice?!'})
+      }
       res.send({ message: 'OK' });
     }
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 
